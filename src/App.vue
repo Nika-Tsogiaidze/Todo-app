dc4a402607e02fc1d33d4cdb6c98c93dce767d44
nika-tsogiaidze <tsogiaidze@yahoo.com>
2018-04-25 00:05:35 +0400

    Working on design

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="container" id="app2">
-    <router-view></router-view>
+    <div class="row">
+      <div class="col-sm-6 tasks_conainer">
+        <div class="row tab_switcher_container">
+          <div class="col-md-6 tab_item active_tab"><router-link to="/active">Active</router-link></div>
+          <div class="col-md-6 tab_item"><router-link to="/archived">Archived</router-link></div>
+        </div>
+            <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,6 +19,9 @@ export default {
   name: 'App',
   components: {
     // HelloWorld
+  }, 
+  created: function () {
+        // alert('nik');
   }
 }
 </script>
@@ -22,5 +33,22 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+.tasks_conainer {
+  margin:0 auto;
+  border-style: solid;
+  border-width: 1px;
+  border-color: #969292;
+}
+.tab_item {
+  background: #867f7f;
+  color: white;
+  cursor: pointer;
+}
+.tab_item a {
+  color:white;
+}
+.tab_item.active_tab {
+  background: #a59b9b;
 }
 </style>
